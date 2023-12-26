08b4ffa4d47bff73622eb4c5a17a53adee12f7fc
tanish vashisth <tanishvashisth@gmail.com>
2023-12-27 02:05:59 +0530

    update

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template("moyemoye.html")
+    now = datetime.datetime.now()
+    current_time = now.strftime(" %I:%M %p")
+    current_date = now.strftime("%a %b %d ")
+    return render_template("moyemoye.html",time=current_time, date=current_date)
 
 
 if __name__ == "__main__":
