bf7da069eaf6e76c819b00f498aa50ffb70b9030
tanish vashisth <tanishvashisth@gmail.com>
2023-12-31 02:29:25 +0530

    moye moye

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ import datetime
 
 app = Flask(__name__)
 
+now = datetime.datetime.now()
+current_time = now.strftime(" %I:%M %p")
+current_date = now.strftime("%a %b %d ")
+
+
 
 @app.route("/")
 def hello():
-    now = datetime.datetime.now()
-    current_time = now.strftime(" %I:%M %p")
-    current_date = now.strftime("%a %b %d ")
     return render_template("moyemoye.html",time=current_time, date=current_date)
 
 
 @app.route("/home")
 def home():
-    return render_template("whttf.html")
+    return render_template("home.html",time=current_time, date=current_date)
 
 if __name__ == "__main__":
     app.run(debug=True)
